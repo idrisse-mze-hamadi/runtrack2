5087393b9179ff8fece19669dddf1a0d930bca7d
idrisse-mze-hamadi <91784605+idrisse-mze-hamadi@users.noreply.github.com>
2021-11-08 12:13:45 +0100

    upload index form
    job01

---
+++ jour04/job01/formulaire.php
@@ -0,0 +1,9 @@
+<?php
+$ca = 0;
+
+while ($_GET == $ca)
+{
+    $ca++;
+}
+echo $ca;
+?>
---
+++ jour04/job01/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    Formulaire
+</head>
+<body>
+
+<form action="formulaire.php" method="get">
+Nom: <input type="text" name="nom"><br>
+Prénom: <input type="text" name="prénom"><br>
+<input type="submit" value="submit">
+</form>
+
+</body>
+</html>
